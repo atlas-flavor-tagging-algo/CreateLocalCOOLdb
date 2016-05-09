8c5c0b833635d8f2d21c294e6377015c8a7b8f8e
Dan Guest <dguest@cern.ch>
2016-05-09 23:15:05 +0200

    Add more options to the root file creater

--- addTextFileToRoot.py
+++ addTextFileToRoot.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+"""
+Add a text file to a ROOT file
+"""
+
 import ROOT
 import sys
 import argparse
@@ -6,28 +12,35 @@ def _run():
     args = _get_args()
 
     if args.input_file.endswith(".root"):
-        print "ERROR: Input file does not match expected format, should be any text format, e.g. JSON or YAML."
+        print "ERROR: Input file should be text format, e.g. JSON or YAML."
         sys.exit(1)
 
     with open(args.input_file,'r') as fp:
         config_str = fp.read()
+    root_file = args.root_file or args.input_file.rsplit(".",1)[0]+".root"
 
-    outHandle = ROOT.TFile.Open(args.input_file.rsplit(".",1)[0]+".root","RECREATE")
+    outHandle = ROOT.TFile.Open(root_file,"RECREATE")
     for i in args.jet_collection:
-        outHandle.mkdir("DL1/"+i)
-        outHandle.cd("DL1/"+i)
+        cal_dir_name = '{}/{}'.format(args.tagger, i)
+        outHandle.mkdir(cal_dir_name)
+        outHandle.cd(cal_dir_name)
         ROOT_str_name = ROOT.TObjString(str(config_str))
-        ROOT_str_name.Write("net_configuration")
+        ROOT_str_name.Write(args.entry_name)
         outHandle.Write()
     outHandle.Close()
 
 def _get_args():
-    help_input_file = "Input file in JSON format using the same conventions as required by Dan's lightweight neural network client (default: %(default)s)."
+    help_input_file = "Input text file"
     help_jet_collection = "Jet collections for which the string will be added (default: %(default)s). Other jet collections like e.g. AntiKt10LCTopo or AntiKt3PV0 can be added as well. Just remember to include \
 them in your job options later when you want to run Athena."
-    parser = argparse.ArgumentParser(description="Showcase the use of argparse on the test case of adding a NN configuration string to a ROOT file for different jet collections.")
-    parser.add_argument("input_file", type=str, default="NNconfig.json",
+    d = '(default: %(default)s)'
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("input_file", type=str,
                         help=help_input_file)
+    parser.add_argument('-r', '--root-file', nargs='?')
+    parser.add_argument('-t', '--tagger', default='DL1', help=d)
+    parser.add_argument('-n', '--entry-name',
+                        default="net_configuration", help=d)
     parser.add_argument("-jc", "--jet_collection", type=str, nargs='+',
                         default=["AntiKt4EMTopo", "AntiKt4LCTopo"],
                         help=help_jet_collection)
